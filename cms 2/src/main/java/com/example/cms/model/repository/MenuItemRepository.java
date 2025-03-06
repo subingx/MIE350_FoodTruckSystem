3fe85421126aac2b4b5309a1abd89b9dc0295ddf
ccccccc223 <leyao.cui@mail.utoronto.ca>
2025-03-06 15:12:38 -0500

    FoodTruckOwner entity done

--- cms 2/src/main/java/com/example/cms/model/repository/MenuItemRepository.java
+++ cms 2/src/main/java/com/example/cms/model/repository/MenuItemRepository.java
@@ -2,6 +2,8 @@ package com.example.cms.model.repository;
 
 import com.example.cms.model.entity.MenuItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
 }
