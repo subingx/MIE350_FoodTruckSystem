ef872bee91c46f73b7cdb9f6e0da76b3a8adb266
SubingXiang <subing.xiang@mail.utoronto.ca>
2025-03-06 13:17:41 -0500

    entity draft3

--- cms 2/src/main/java/com/example/cms/model/entity/FoodTruck.java
+++ cms 2/src/main/java/com/example/cms/model/entity/FoodTruck.java
@@ -1,4 +1,37 @@
 package com.example.cms.model.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.lang.Nullable;
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "food_trucks")
 public class FoodTruck {
+
+    @Id
+    @NotEmpty
+    private String code;
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private String location;
+
+    @No
+
+
+    public Department(String code, String name, String contactEmail){
+        this.code = code;
+        this.name = name;
+        this.contactEmail = contactEmail;
+    }
+
 }
