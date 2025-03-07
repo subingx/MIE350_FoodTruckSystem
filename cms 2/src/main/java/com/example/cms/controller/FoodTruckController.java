128e29358906c8d5168df4fb93161e7a01e4261f
kry <kry@gmail.com>
2025-03-06 22:02:35 -0500

    foodtruck controller

--- cms 2/src/main/java/com/example/cms/controller/FoodTruckController.java
+++ cms 2/src/main/java/com/example/cms/controller/FoodTruckController.java
@@ -1,4 +1,71 @@
 package com.example.cms.controller;
 
+import com.example.cms.controller.dto.CourseDto;
+import com.example.cms.controller.exceptions.DepartmentNotFoundException;
+import com.example.cms.controller.exceptions.ProfessorNotFoundException;
+import com.example.cms.model.entity.*;
+import com.example.cms.model.repository.*;
+
+import com.example.cms.controller.exceptions.FoodTruckNotFoundException;
+import com.example.cms.controller.exceptions.FoodTruckOwnerNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
 public class FoodTruckController {
+    @Autowired
+    private final FoodTruckRepository foodTruckRepository;
+
+    @Autowired
+    private FoodTruckOwnerRepository OwnerRepository;
+
+    public FoodTruckController(FoodTruckRepository repository) {
+        this.foodTruckRepository = repository;
+    }
+
+
+
+    @GetMapping("/departments")
+    List<Department> retrieveAllDepartments() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/departments/{code}")
+    Department retrieveDepartment(@PathVariable("code") String departCode) {
+        return repository.findById(departCode)
+                .orElseThrow(() -> new DepartmentNotFoundException(departCode));
+    }
+
+    //
+    @Autowired
+    private final CourseRepository repository;
+
+    @Autowired
+    private ProfessorRepository professorRepository;
+
+    public CourseController(CourseRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/courses")
+    List<Course> retrieveAllCourses() {
+        return repository.findAll();
+    }
+
+    @PostMapping("/courses")
+    Course createCourse(@RequestBody CourseDto courseDto) {
+        Course newCourse = new Course();
+        newCourse.setName(courseDto.getName());
+        newCourse.setCode(courseDto.getCode());
+        Professor professor = professorRepository.findById(courseDto.getProfessorId()).orElseThrow(
+                () -> new ProfessorNotFoundException(courseDto.getProfessorId()));
+        newCourse.setProfessor(professor);
+        return repository.save(newCourse);
+    }
+
+
 }
--- cms 2/src/main/java/com/example/cms/model/repository/MenuItemRepository.java
+++ cms 2/src/main/java/com/example/cms/model/repository/MenuItemRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
+public interface MenuItemRepository extends JpaRepository<MenuItem, String> {
 }
